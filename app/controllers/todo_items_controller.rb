517f0a2de5d4f18c4f41254aa3d08851e381b11a
Misha <mishakib91@icloud.com>
2015-11-19 09:42:08 +0200

    Finished app

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -18,7 +18,12 @@ class TodoItemsController < ApplicationController
 
   def complete
     @todo_item.update_attribute(:completed_at, Time.now)
-    redirect_to @todo_list, notice: "Todo item completed"
+    redirect_to @todo_list
+  end
+
+  def not_complete
+    @todo_item.update_attribute(:completed_at, nil)
+    redirect_to @todo_list
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     resources :todo_items do
       member do
         patch :complete
+        patch :not_complete
       end
     end
   end
